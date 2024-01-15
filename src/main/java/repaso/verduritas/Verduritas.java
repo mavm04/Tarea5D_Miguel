7dc801e51ea1ad6a537b084b2ccfd2e4dd9d050b
mavm0406 <mavm040601@gmail.com>
2024-01-15 09:59:02 +0100

    Añade atributos en clase modelo Añade constructores en clase modelo

---
+++ src/main/java/repaso/verduritas/Verduritas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package repaso.verduritas;
+
+/**
+ *
+ * @author miguel
+ */
+public class Verduritas {
+
+    private String codVerdurita;
+    private String nombre;
+
+    public Verduritas(String codVerdurita, String nombre) {
+        this.codVerdurita = codVerdurita;
+        this.nombre = nombre;
+    }
+
+    public Verduritas() {
+    } 
+    
+    
+}
