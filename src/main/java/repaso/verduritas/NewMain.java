2f7467305c9c63dd663a8d9ca00d599124cf583a
clara <calvara976@g.educaand.es>
2024-01-15 10:12:20 +0100

    creamos verduras y las mostramos

---
+++ src/main/java/repaso/verduritas/NewMain.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repaso.verduritas;
+
+/**
+ *
+ * @author clara
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // creamos objetos y los mostramos
+        Verduritas v1 = new Verduritas("123", "pimiento");
+        Verduritas v2 = new Verduritas("88", "calabaza");
+
+        System.out.println("Primera verdura " + v1);
+        System.out.println("Segunda verdura " + v2);
+
+    }
+
+}
